4950a94b43b20a2ba69e431488039d7de4a6ab93
haythemchagwey <haythemchagwey@gmail.com>
2018-11-25 18:27:55 +0100

    fix duplication

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import routes from './routes';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 
 const app = express();
-dotenv.config({ path: 'variables.env' });
 
 app.use(cors());
 
@@ -17,13 +14,4 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
-mongoose.connect(
-  process.env.DATABASE,
-  { useNewUrlParser: true }
-);
-
-app.set('port', process.env.PORT || 8080);
-
-const server = app.listen(app.get('port'), () => {
-  console.log(`server is running on ${server.address().port}`);
-});
+export default app;
